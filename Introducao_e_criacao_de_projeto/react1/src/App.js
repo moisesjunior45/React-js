933bdc1801c95d624189eddd4bfc8e7144c250b0
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2023-04-03 16:24:58 -0300

    Editando arquivos

--- Introducao_e_criacao_de_projeto/react1/src/App.js
+++ Introducao_e_criacao_de_projeto/react1/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+// AULA 1 E 2
+// No React existem 2 formas de criar um componente, estas são:
+
+// 1º
+/*class App extends React.Component {
+  render() {
+    return <h1>Testando 1,2,3...</h1>;
+  }
+}*/
+
+// 2º
+/*function App() {
+  return <h1>Testando 3,4,5</h1>;
+}*/
+
+// Utilizando função anônima
+/*let App = () => {
+  return <h1>Testando 6,7,8</h1>;
+};*/
+
+// Utilizando arrow function
+// let App = () => <h1>Testando 9,10,11...</h1>;
+
+// AULA 3
+function formatarNome(usuario) {
+  return usuario.nome + ' ' + usuario.sobrenome;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Opa, tudo bem?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let usuario = {
+    nome: 'Moisés',
+    sobrenome: 'Oliveira'
+  };
+
+  let imagem = 'https://www.google.com.br/google.jpg';
+
+  return <>
+    <div>Meu nome é {formatarNome(usuario)}</div>
+    <img src={imagem} />
+  </>;
 }
 
 export default App;
