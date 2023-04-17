1bb99a7ffd3d19f5774f58f3515b95dda25a0ee8
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2023-04-17 16:03:36 -0300

    Editando arquivo

--- Conceitos-basicos/react1/src/App.js
+++ Conceitos-basicos/react1/src/App.js
@@ -23,8 +23,8 @@ import React from "react";
 // Utilizando arrow function
 // let App = () => <h1>Testando 9,10,11...</h1>;
 
-// AULA 3
-function formatarNome(usuario) {
+// AULA 3 - Entendendo o JSX
+/*function formatarNome(usuario) {
   return usuario.nome + ' ' + usuario.sobrenome;
 }
 
@@ -40,6 +40,32 @@ function App() {
     <div>Meu nome é {formatarNome(usuario)}</div>
     <img src={imagem} />
   </>;
+}*/
+
+
+// Aula 04 - Componentes e Props
+
+function Avatar(props) {
+  return (
+    <div>
+      <img src={props.user.url} alt={props.user.name} />
+      <br />
+      <span>{props.user.name}</span>
+    </div>
+  );
 }
+
+function App() {
+  
+  let user = {
+    url:"https://avatars.githubusercontent.com/u/84156546?s=400&u=43abd60f780d76a7d54e753c622f26158d6096ed&v=4",
+    name:"Moisés Júnior"
+  }
+  return <>
+    <Avatar user={user} />
+  </>;
+}
+
+
 
 export default App;
